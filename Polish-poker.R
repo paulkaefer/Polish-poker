bd4d3354e6ec28b19bb40ecb4766b8a3806ec34e
Paul Kaefer <paul.kaefer@gmail.com>
2016-02-29 18:31:23 -0600

    began simulation file in R

---
+++ Polish-poker.R
@@ -0,0 +1,14 @@
+# easier to store cards as numbers (11=Jack... 14=Joker)
+cards <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+nCards <- length(cards)
+
+# this depends on how you play. there seem to be several different scoring systems.
+pointValues <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 0, -3)
+
+nTrials <- 10000
+
+for (i in 1:nTrials) {
+  
+  
+  
+}
